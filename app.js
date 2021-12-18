f94e46783043280eaafbcb8b217627ba3efb74e9
oh-yeah-sea-kit2 <kyohei.t.204@gmail.com>
2021-12-18 15:43:54 +0900

    fix: scope

--- app.js
+++ app.js
@@ -188,7 +188,13 @@ const provision = async () => {
     password: "cookie_encryption_password_secure",
     clientId: process.env.SLACK_CLIENT_ID,
     clientSecret: process.env.SLACK_CLIENT_SECRET,
-    scope: ["commands", "chat:write", "emoji:read"],
+    scope: [
+      'commands',
+      'chat:write',
+      'emoji:read',
+      'chat:write.customize',
+      'chat:write.public'
+    ],
     isSecure: false,
     forceHttps: true,
   });
